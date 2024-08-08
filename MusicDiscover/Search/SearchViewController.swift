f73308285145241c11a001b2ace12333f9e78e74
hwanghye <hwang.sine@gmail.com>
2024-08-08 18:26:56 +0900

    initial commit

---
+++ MusicDiscover/Search/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  MusicDiscover
+//
+//  Created by hwanghye on 8/8/24.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
